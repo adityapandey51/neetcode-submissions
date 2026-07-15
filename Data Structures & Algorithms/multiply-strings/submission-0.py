875b44291b4bc38d403c5f46d1b60deeab66b4e4
Aditya Pandey <124462709+adityapandey51@users.noreply.github.com>
2026-07-16 00:30:29 +0530

    Add: multiply-strings - submission-0

---
+++ Data Structures & Algorithms/multiply-strings/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        if num1 == "0" or num2 =="0": return "0"
+        num1 = num1[::-1]
+        num2 = num2[::-1]
+
+        res = [0]*(len(num1)+len(num2))
+
+        for i in range(len(num1)):
+            for j in range(len(num2)):
+                res[i+j] += int(num1[i])*int(num2[j])
+                res[i+j+1] += res[i+j]//10
+                res[i+j] = res[i+j]%10
+        res, beg = res[::-1],0
+        for i in range(len(res)):
+            if res[i]==0:
+                beg+=1
+            else: break
+        return "".join(map(str,res[beg:]))
+        
